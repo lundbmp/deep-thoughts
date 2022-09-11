9220de68f120fceace318cb168ebac961272abab
lundbmp <m.lundberg008@gmail.com>
2022-09-11 18:27:01 -0500

    added add user and login capabilities

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require("../models");
+const { authenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
@@ -23,16 +24,28 @@ const resolvers = {
         .populate("friends")
         .populate("thoughts");
     },
-    Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
 
-            return user;
-        },
-        login: async (parent, args) => {
+      return user;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-        }
-    }
+      if (!user) {
+        throw new authenticationError("incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new authenticationError("incorrect credentials");
+      }
+
+      return user;
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -35,8 +35,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String!): User
     addUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): User
   }
 `;
 
